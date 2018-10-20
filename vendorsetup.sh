4331f35158af225dfe755bd8ff4dfc6a68c9d552
Maanush2004 <p.maanush@gmail.com>
2018-10-20 17:25:42 +0530

    Apply Patches with superuser priveleges
    For Some people, some patches are giving permission denied errors. This should remove those errors.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -25,26 +25,26 @@ for var in eng user userdebug; do
 done
 
 cd frameworks/av
-git reset --hard && git clean -f -d
-git apply -v ../../device/lenovo/marino/patches/0001-frameworks_av.patch
+sudo git reset --hard && sudo git clean -f -d
+sudo git apply -v ../../device/lenovo/marino/patches/0001-frameworks_av.patch
 cd ../..
 
 cd frameworks/base
-git reset --hard && git clean -f -d
-git apply -v ../../device/lenovo/marino/patches/0002-frameworks_base.patch
+sudo git reset --hard && sudo git clean -f -d
+sudo git apply -v ../../device/lenovo/marino/patches/0002-frameworks_base.patch
 cd ../..
 
 cd frameworks/native
-git reset --hard && git clean -f -d
-git apply -v ../../device/lenovo/marino/patches/0003-frameworks_native.patch
+sudo git reset --hard && sudo git clean -f -d
+sudo git apply -v ../../device/lenovo/marino/patches/0003-frameworks_native.patch
 cd ../..
 
 cd system/netd
-git reset --hard && git clean -f -d
-git apply -v ../../device/lenovo/marino/patches/0004-system_netd.patch
+sudo git reset --hard && sudo git clean -f -d
+sudo git apply -v ../../device/lenovo/marino/patches/0004-system_netd.patch
 cd ../..
 
 cd system/core
-git reset --hard && git clean -f -d
-git apply -v ../../device/lenovo/marino/patches/0005-system_core.patch
+sudo git reset --hard && sudo git clean -f -d
+sudo git apply -v ../../device/lenovo/marino/patches/0005-system_core.patch
 cd ../..
