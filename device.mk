1c60f2af7a269422426f87bd4c4bb3c53d0b8e2a
Maanush Putcha <p.maanush@gmail.com>
2019-07-26 15:22:00 +0530

    marino: Add stub_audio_policy_configuration.xml
    * Needed after https://github.com/LineageOS/android_frameworks_av/commit/40736eaee8e0f4d0fd359c1d4cd7dadb54807419 i.e starting from Oreo-MR1
    
    Signed-off-by: Maanush Putcha <p.maanush@gmail.com>

--- device.mk
+++ device.mk
@@ -43,6 +43,7 @@ PRODUCT_COPY_FILES += \
 	frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:system/vendor/etc/audio_policy_volumes.xml \
 	frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/vendor/etc/default_volume_tables.xml \
 	frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/vendor/etc/r_submix_audio_policy_configuration.xml \
+	frameworks/av/services/audiopolicy/config/stub_audio_policy_configuration.xml:system/vendor/etc/stub_audio_policy_configuration.xml \
 	frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/vendor/etc/usb_audio_policy_configuration.xml 
 
 # Wi-Fi Configs
