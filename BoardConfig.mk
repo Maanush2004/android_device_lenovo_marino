74eeadb36428ac4bfad657b82111239678eddd61
Maanush Putcha <p.maanush@gmail.com>
2020-04-15 17:47:44 +0000

    marino: Initial TWRP Tree
    * Recovery has LZMA Ramdisk
    * MTP not working
    * Decryption not working
    
    Signed-off-by: Maanush Putcha <p.maanush@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,93 @@
+#
+# Copyright 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6757
+TARGET_NO_BOOTLOADER := true		
+TARGET_NO_FACTORYIMAGE := true
+TARGET_BOOTLOADER_BOARD_NAME := mt6757
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+TARGET_BOARD_SUFFIX := _64
+TARGET_BOARD_PLATFORM_GPU := Mali-T880MP2
+TARGET_USES_64_BIT_BINDER := true
+
+TARGET_CPU_ABI_LIST_64_BIT := $(TARGET_CPU_ABI)
+TARGET_CPU_ABI_LIST_32_BIT := $(TARGET_2ND_CPU_ABI),$(TARGET_2ND_CPU_ABI2)
+TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI_LIST_64_BIT),$(TARGET_CPU_ABI_LIST_32_BIT)
+
+# Architecture Extensions
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_HAVE_VFP := true
+
+# File System
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Kernel
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_SOURCE := kernel/lenovo/marino
+TARGET_KERNEL_CONFIG := marino-twrp_defconfig
+BOARD_KERNEL_BASE := 0x40078000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_RAMDISK_OFFSET := 0x04f88000
+BOARD_TAGS_OFFSET := 0x03f88000
+BOARD_SECOND_OFFSET := 0x00e88000
+BOARD_KERNEL_OFFSET = 0x00008000
+TARGET_KERNEL_ARCH := arm64
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
+BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --second_offset $(BOARD_SECOND_OFFSET) --tags_offset $(BOARD_TAGS_OFFSET)
+
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+RECOVERY_SDCARD_ON_DATA := true
+
+# Resolution
+DEVICE_RESOLUTION := 1080x1920
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
+
+# TWRP
+TW_THEME := portrait_hdpi
+TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_EXCLUDE_SUPERSU := true
+TW_INCLUDE_FB2PNG := true
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TW_NO_BATT_PERCENT := true
+TW_REBOOT_RECOVERY := true
+TW_HAVE_SELINUX := true
+TW_DEFAULT_LANGUAGE := en
+TARGET_RECOVERY_FSTAB := device/lenovo/marino/recovery.fstab
+LZMA_RAMDISK_TARGETS := recovery
